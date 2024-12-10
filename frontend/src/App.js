0cf8b4129257fbc7f898a512e447c1bb132eac91
matsu128 <matsumoto1282002@icloud.com>
2024-12-10 22:55:23 +0900

    #4 feat: ヘッダーのデザインの実装。iconに関して、jwtを元にログインしていたらその人のiconを表示する処理が未実装。ヘッダーは検索画面とチャット画面で表示する。App.jsでHeaderを管理する。

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,39 @@ import UserSearchPage from './pages/UserSearch/UserSearchPage';
 import ChatPage from './pages/Chat/ChatPage';
 import MyPage from './pages/MyPage/MyPage';
 import LoginPage from './pages/Login/LoginPage';
+import Header from './atoms/Header'; // Headerをインポート
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserSearchPage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        {/* '/' パスに対して Header を表示 */}
+        <Route 
+          path="/" 
+          element={
+            <>
+              <Header isLoggedIn={false} /> {/* ヘッダーの表示 */}
+              <div className="mt-16"> {/* ヘッダーの高さ分だけ余白を追加 */}
+                <UserSearchPage /> {/* ページのコンテンツ */}
+              </div>
+            </>
+          } 
+        />
+        
+        {/* '/chat' パスに対して Header を表示 */}
+        <Route 
+          path="/chat" 
+          element={
+            <>
+              <Header isLoggedIn={false} /> {/* ヘッダーの表示 */}
+              <div className="mt-16"> {/* ヘッダーの高さ分だけ余白を追加 */}
+                <ChatPage /> {/* チャットページのコンテンツ */}
+              </div>
+            </>
+          } 
+        />
+        
+        {/* その他のページ */}
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
